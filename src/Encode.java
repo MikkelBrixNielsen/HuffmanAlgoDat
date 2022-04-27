f853db205eb45a5c2432930a1b4d9143cbb6def5
MikkelBN <mikkelbn24@live.dk>
2022-04-27 13:05:48 +0200

    Initial commit

---
+++ src/Encode.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.nio.file.Path;
+
+public class Encode {
+    public static void main(String[] args) {
+        Path inputFile = Path.of(args[0]);
+        Path outputFile = Path.of(args[1]);
+
+    
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
