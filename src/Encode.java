e296fc934fd194bbc549f9685f7c8ee53b7dceb9
suiziide <nicolai9595@hotmail.com>
2022-05-03 13:45:49 +0200

    Fixed imports

--- src/Encode.java
+++ src/Encode.java
@@ -1,7 +1,4 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class Encode {
     public static void main(String[] args) {
